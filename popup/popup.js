ffaf3367c0536877200867db285f43090640177e
Theo <theo.markkus.paul@gmail.com>
2022-02-25 16:22:16 -0800

    Works pretty dece now

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,54 @@
-console.error('here')
+// TODO: put these somewhere
+const mdnBaseURL = 'https://developer.mozilla.org';
+const searchIndexUrl = 'https://developer.mozilla.org/en-US/search-index.json';
 
-const searchIndexUrl = 'https://developer.mozilla.org/en-US/search-index.json'
-const searchInput = $('#search');
-const title = document.getElementById('title');
+// Taken from the examples page from the twitter typeahead library
+// https://twitter.github.io/typeahead.js/examples/#the-basics
+var substringMatcher = function(mdnPages) {
+    return function findMatches(q, cb) {
+      var matches, substringRegex;
+  
+      // an array that will be populated with substring matches
+      matches = [];
+  
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, 'i');
+  
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(mdnPages, function(i, mdnPage) {
+        if (substrRegex.test(mdnPage.title)) {
+          matches.push(mdnPage);
+        }
+      });
+  
+      cb(matches);
+    };
+  };
 
-// Nice, you've just reinvented a SPA
-searchInput.on('change', event => {
-    console.log(data)
-    title.textContent = event.target.value;
-});
+$(document).ready(() => {
+    $('.typeahead-container .typeahead-search').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+        display: 'title',
+        source: substringMatcher(data),
+        templates: {
+            empty: [
+            '<div class="empty-message">',
+                'unable to find any results that match the current query.',
+            '</div>'
+            ].join('\n'),
+            suggestion: function(data) {
+                return '<div><strong><a href="' + mdnBaseURL + data.url + '#syntax' + '">' + data.title + '<a/></strong><p style="font-size: 14px;">' + data.url.split('/').join(' / ') + '</p></div>';
+              }
+        }
+    });
+
+    $('.typeahead-container .typeahead-search').on('typeahead:select', (_event, suggestion) => {
+        browser.tabs.create({ url: mdnBaseURL + suggestion.url});
+        window.close();
+    });
+})
